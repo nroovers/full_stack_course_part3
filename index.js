0ec9a4f75a3c0f5fce7748deadac019951edebd0
Nicolai Roovers <nroovers@gmail.com>
2019-11-13 20:48:57 +0200

    exercise3.3

--- index.js
+++ index.js
@@ -42,6 +42,15 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    console.log(`call ${req.originalUrl}`)
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        res.json(person)
+    }
+    else { res.status(404).end() }
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
