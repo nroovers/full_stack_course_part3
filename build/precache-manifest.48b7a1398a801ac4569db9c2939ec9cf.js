1c3eb5462899a2f44cbe0627005893ed5582c13c
Nicolai Roovers <nroovers@gmail.com>
2019-11-14 17:39:04 +0200

    uibuild

---
+++ build/precache-manifest.48b7a1398a801ac4569db9c2939ec9cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a498bf16a15bf7e4f98eb81b6861c03e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f413a697508e2a664303",
+    "url": "/static/css/main.b0889fd5.chunk.css"
+  },
+  {
+    "revision": "59cbbd3bbd514db0a2fe",
+    "url": "/static/js/2.fe1174d5.chunk.js"
+  },
+  {
+    "revision": "f413a697508e2a664303",
+    "url": "/static/js/main.14f90985.chunk.js"
+  },
+  {
+    "revision": "4e646f0e581801a589fc",
+    "url": "/static/js/runtime-main.7767e197.js"
+  }
+]);
