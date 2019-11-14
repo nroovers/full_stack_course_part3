de1e4ec18485bf1203dedefecfd32c7f41460365
Nicolai Roovers <nroovers@gmail.com>
2019-11-14 10:45:50 +0200

    exercise3.8

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', function (req, res) {
+    return req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
 let persons =
